0304ea61080b8b68c7a2fa6e268d2804bf4e34e6
casobri17 <casobri17@gmail.com>
2024-11-26 12:30:27 -0600

    iuefhef

--- sketch.js
+++ sketch.js
@@ -1,48 +1,57 @@
+let armOffset = 0;
+let legOffset = 0;
+let dancingUp = true;
+
 function setup() {
     createCanvas(600, 600);
-    noLoop(); // Only draw on trigger (key press)
+    background (220);
+    noStroke();
+    drawCreature(width/2, heigh/2, 100);
   }
   
-  function drawCreature(x, y, size, headColor, bodyColor, eyeColor, numEyes) {
-    // Draw body
-    fill(bodyColor);
-    ellipse(x, y + size * 0.5, size, size * 0.6);
+  function drawCreature(x, y, size) 
+    {
+    //Body
+    fill(150, 200, 255); //blue
+    ellipse(x, y + size * 0.4, size * 0.8, size);
   
-    // Draw head
+    //Head
     fill(headColor);
     ellipse(x, y, size * 0.7, size * 0.7);
   
-    // Draw eyes based on numEyes input
-    fill(eyeColor);
-    for (let i = 0; i < numEyes; i++) {
-      let eyeX = x - (numEyes - 1) * 10 + i * 20;
-      ellipse(eyeX, y - 10, size * 0.1, size * 0.1);
+    //Eyes
+    fill(0); //black
+    ellipse(x - size * 0.15, y - size * 0.1, size * 0.1, size * 0.1); //left eye
+    ellipse(x + size * 0.15, y - size * 0.1, size * 0.1, size * 0.1); //right eye
+
+    //Arms
+    fill(150, 200, 255);
+    ellipse(x - size * 0.6, y + size * 0.2 + armOffset, size * 0.2, size * 0.4); //left arm
+    ellipse(x + size * 0.6, y + size * 0.2 - armOffset, size * 0.2, size * 0.4); //right arm
+    
+    //Legs
+    ellipse(x - size * 0.25, y + size * 0.8 + lefOffset, size * 0.2, size * 0.2); //left leg
+    ellipse(x + size * 0.25, y + size * 0.8 - lefOffset, size * 0.2, size * 0.2); //left leg
+
+    //Mouth
+    fill(255, 100, 100); //red
+    arc(x, y + size * 0.1, size * 0.4, size * 0.2, 0, PI); //smile
     }
   
-    // Draw legs based on size input
-    fill(bodyColor);
-    for (let i = -1; i <= 1; i += 2) {
-      ellipse(x + i * (size * 0.4), y + size * 0.8, size * 0.1, size * 0.3);
+function mousePressed()
+{
+    background(220);
+
+    if (dancingUp) {
+        armOffset = 20;
+        legOffset = -20;
     }
-  }
-  
-  // Trigger new creature generation
-  function keyPressed() {
-    if (key === ' ') {
-      // Randomize background color
-      background(random(255), random(255), random(255));
-  
-      // Randomize creature properties
-      let x = random(width * 0.3, width * 0.7);
-      let y = random(height * 0.3, height * 0.7);
-      let size = random(50, 150);
-      let headColor = color(random(255), random(255), random(255));
-      let bodyColor = color(random(255), random(255), random(255));
-      let eyeColor = color(random(255), random(255), random(255));
-      let numEyes = int(random(1, 5)); // Choose 1-4 eyes
-  
-      // Draw a new creature with random properties
-      drawCreature(x, y, size, headColor, bodyColor, eyeColor, numEyes);
+    else {
+        armOffset = -20;
+        legOffset = 20;
     }
-  }
-  
+
+    dancingUp = !dancingUp;
+
+    drawCreature(width / 2, height / 2, 100);
+}
